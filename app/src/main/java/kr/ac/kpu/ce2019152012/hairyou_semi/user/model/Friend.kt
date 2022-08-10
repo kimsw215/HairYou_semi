78c377d875a72bbc279671ea7f9a9bea44061b5f
SEONMl <sm000117@naver.com>
2022-08-10 21:37:24 +0900

    add models

---
+++ app/src/main/java/kr/ac/kpu/ce2019152012/hairyou_semi/user/model/Friend.kt
@@ -0,0 +1,8 @@
+package kr.ac.kpu.ce2019152012.hairyou_semi.user.model
+
+data class Friend(
+    val email : String? = null,
+    val name : String? = null,
+    val profileImageUrl : String? = null,
+    val uid : String? = null
+)
