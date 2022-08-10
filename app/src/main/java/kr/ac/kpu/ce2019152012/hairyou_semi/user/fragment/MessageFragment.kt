b2b835072b56269e9e0c79691fedd305d6e9dc35
SEONMl <sm000117@naver.com>
2022-08-10 21:40:51 +0900

    add chatting feature

--- app/src/main/java/kr/ac/kpu/ce2019152012/hairyou_semi/user/fragment/MessageFragment.kt
+++ app/src/main/java/kr/ac/kpu/ce2019152012/hairyou_semi/user/fragment/MessageFragment.kt
@@ -1,25 +1,54 @@
 package kr.ac.kpu.ce2019152012.hairyou_semi.user.fragment
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+import kr.ac.kpu.ce2019152012.hairyou_semi.R
 import kr.ac.kpu.ce2019152012.hairyou_semi.databinding.FragmentMessageBinding
+import kr.ac.kpu.ce2019152012.hairyou_semi.user.MessageActivity
+import kr.ac.kpu.ce2019152012.hairyou_semi.user.model.ChatModel
+import kr.ac.kpu.ce2019152012.hairyou_semi.user.model.Friend
+import java.util.*
 
-class MessageFragment : Fragment(){
+class MessageFragment : Fragment() {
+    companion object {
+        fun newInstance(): MessageFragment {
+            return MessageFragment()
+        }
+    }
+
+    private val fireDatabase = FirebaseDatabase.getInstance().reference
+
     //프래그먼트가 메모리에 올라갔을때
     private var _binding: FragmentMessageBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var navController : NavController
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
+    // 프래그먼트를 포함하고 있는 액티비티에 붙었을 때
     override fun onAttach(context: Context) {
         super.onAttach(context)
     }
@@ -34,8 +63,97 @@ class MessageFragment : Fragment(){
 
         _binding = FragmentMessageBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.chatfragment_recyclerview)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = RecyclerViewAdapter()
+
         return view
     }
+
+    inner class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.CustomViewHolder>() {
+
+        private val chatModel = ArrayList<ChatModel>()
+        private var uid: String? = null
+        private val destinationUsers: ArrayList<String> = arrayListOf()
+
+        init {
+            uid = Firebase.auth.currentUser?.uid.toString()
+            println(uid)
+
+            fireDatabase.child("chatrooms").orderByChild("users/$uid").equalTo(true)
+                .addListenerForSingleValueEvent(object :
+                    ValueEventListener {
+                    override fun onCancelled(error: DatabaseError) {
+                    }
+
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        chatModel.clear()
+                        for (data in snapshot.children) {
+                            chatModel.add(data.getValue<ChatModel>()!!)
+                            println(data)
+                        }
+                        notifyDataSetChanged()
+                    }
+                })
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
+
+            return CustomViewHolder(
+                LayoutInflater.from(context).inflate(R.layout.item_chat, parent, false)
+            )
+        }
+
+        inner class CustomViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val imageView: ImageView = itemView.findViewById(R.id.chat_item_imageview)
+            val textView_title: TextView = itemView.findViewById(R.id.chat_textview_title)
+            val textView_lastMessage: TextView =
+                itemView.findViewById(R.id.chat_item_textview_lastmessage)
+        }
+
+
+        override fun onBindViewHolder(holder: RecyclerViewAdapter.CustomViewHolder, position: Int) {
+            var destinationUid: String? = null
+            //채팅방에 있는 유저 모두 체크
+            for (user in chatModel[position].users.keys) {
+                if (!user.equals(uid)) {
+                    destinationUid = user
+                    destinationUsers.add(destinationUid)
+                }
+            }
+            fireDatabase.child("users").child("$destinationUid")
+                .addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onCancelled(error: DatabaseError) {
+                    }
+
+                    override fun onDataChange(snapshot: DataSnapshot) {
+                        val friend = snapshot.getValue<Friend>()
+                        Glide.with(holder.itemView.context).load(friend?.profileImageUrl)
+                            .apply(RequestOptions().circleCrop())
+                            .into(holder.imageView)
+                        holder.textView_title.text = friend?.name
+                    }
+                })
+            //메세지 내림차순 정렬 후 마지막 메세지의 키값을 가져옴
+            val commentMap = TreeMap<String, ChatModel.Comment>(Collections.reverseOrder())
+            commentMap.putAll(chatModel[position].comments)
+            val lastMessageKey = commentMap.keys.toTypedArray()[0]
+            holder.textView_lastMessage.text = chatModel[position].comments[lastMessageKey]?.message
+
+            //채팅창 선책 시 이동
+            holder.itemView.setOnClickListener {
+                val intent = Intent(context, MessageActivity::class.java)
+                intent.putExtra("destinationUid", destinationUsers[position])
+                context?.startActivity(intent)
+            }
+        }
+
+        override fun getItemCount(): Int {
+            return chatModel.size
+        }
+    }
+
 
     // 코딩
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
--- app/src/main/java/kr/ac/kpu/ce2019152012/hairyou_semi/user/fragment/MypageFragment.kt
+++ app/src/main/java/kr/ac/kpu/ce2019152012/hairyou_semi/user/fragment/MypageFragment.kt
@@ -1,14 +1,51 @@
 package kr.ac.kpu.ce2019152012.hairyou_semi.user.fragment
 
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import kr.ac.kpu.ce2019152012.hairyou_semi.R
 import kr.ac.kpu.ce2019152012.hairyou_semi.databinding.FragmentMypageBinding
 
+import kr.ac.kpu.ce2019152012.hairyou_semi.user.model.Friend
+
 class MypageFragment : Fragment(){
+    companion object{
+        private var imageUri : Uri? = null
+        private val fireStorage = FirebaseStorage.getInstance().reference
+        private val fireDatabase = FirebaseDatabase.getInstance().reference
+        private val user = Firebase.auth.currentUser
+        private val uid = user?.uid.toString()
+        fun newInstance() : MypageFragment {
+            return MypageFragment()
+        }
+    }
+
+
     //프래그먼트가 메모리에 올라갔을때
     private var _binding: FragmentMypageBinding? = null
     private val binding get() = _binding!!
@@ -24,8 +61,80 @@ class MypageFragment : Fragment(){
         binding.customerMypageName.text = "이름: ${"name"}"
         binding.customerMypagePhone.text = "전화번호: ${"phone"}"
 
+        // 추가한 부분
+        //view 선언을 안하고 return에 바로 적용시키면 glide가 작동을 안함
+        val photo = view?.findViewById<ImageView>(R.id.profile_imageview)
+
+        val email = view?.findViewById<TextView>(R.id.profile_textview_email)
+        val name = view?.findViewById<TextView>(R.id.profile_textview_name)
+        val button = view?.findViewById<Button>(R.id.profile_button)
+
+        //프로필 구현
+        fireDatabase.child("users").child(uid).addListenerForSingleValueEvent(object :
+            ValueEventListener {
+            override fun onCancelled(error: DatabaseError) {
+            }
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val userProfile = snapshot.getValue<Friend>()
+                println(userProfile)
+                Glide.with(requireContext()).load(userProfile?.profileImageUrl)
+                    .apply(RequestOptions().circleCrop())
+                    .into(photo!!)
+                email?.text = userProfile?.email
+                name?.text = userProfile?.name
+            }
+        })
+        //프로필사진 바꾸기
+        photo?.setOnClickListener{
+            val intentImage = Intent(Intent.ACTION_PICK)
+            intentImage.type = MediaStore.Images.Media.CONTENT_TYPE
+            //getContent.launch(intentImage)
+        }
+        button?.setOnClickListener{
+            if(name?.text!!.isNotEmpty()) {
+                fireDatabase.child("users/$uid/name").setValue(name.text.toString())
+                name.clearFocus()
+                Toast.makeText(requireContext(), "이름이 변경되었습니다.", Toast.LENGTH_SHORT).show()
+            }
+        }
+
         return view
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onAttach(activity: Activity) {
+        super.onAttach(activity)
+    }
+
+    /*
+    private val getContent =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result: ActivityResult ->
+            if(result.resultCode == AppCompatActivity.RESULT_OK) {
+                imageUri = result.data?.data //이미지 경로 원본
+                profile_imageview.setImageURI(imageUri) //이미지 뷰를 바꿈
+
+                //기존 사진을 삭제 후 새로운 사진을 등록
+                fireStorage.child("userImages/$uid/photo").delete().addOnSuccessListener {
+                    fireStorage.child("userImages/$uid/photo").putFile(imageUri!!).addOnSuccessListener {
+                        fireStorage.child("userImages/$uid/photo").downloadUrl.addOnSuccessListener {
+                            val photoUri : Uri = it
+                            println("$photoUri")
+                            fireDatabase.child("users/$uid/profileImageUrl").setValue(photoUri.toString())
+                            Toast.makeText(requireContext(), "프로필사진이 변경되었습니다.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+                Log.d("이미지", "성공")
+            }
+            else{
+                Log.d("이미지", "실패")
+            }
+        }
+
+     */
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
